e7258ef84b108124847736ac2eae9b7e6eef80c7
Ambrož Perovšek <64433302+PotatoHunter12@users.noreply.github.com>
2024-04-20 13:21:42 +0200

    Started project in vue

---
+++ nekizelektriko/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <h1>Neki z elektriko</h1>
+  <RouterView />
+</template>
+
+<style scoped>
+
+h1 {
+  text-align: center;
+  color: cornflowerblue;
+}
+
+@media (min-width: 1024px) {
+  /* css za web */
+  h1 {
+    color: red;
+  }
+}
+</style>
