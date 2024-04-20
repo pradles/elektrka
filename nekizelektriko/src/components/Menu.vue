36686cc51f64cb9dfbf66db6eafbff9d027ab44a
pradles <jernej.ivancic2002@gmail.com>
2024-04-20 21:55:43 +0200

    left-menu

---
+++ nekizelektriko/src/components/Menu.vue
@@ -0,0 +1,79 @@
+<template>
+  
+<button @click="toggleSidebar" aria-controls="logo-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200">
+    <span class="sr-only">Open sidebar</span>
+    <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+    </svg>
+</button>
+
+<aside id="default-sidebar" :class="{'-translate-x-full': !isSidebarOpen, 'translate-x-0': isSidebarOpen}" class="fixed top-0 sm:left-12 left-0 z-40 sm:w-72 w-64 h-screen transition-transform sm:transition-transform sm:duration-500 bg-white" aria-label="Sidebar">
+    <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 sm:border-l-4 border-l-0 border-r-4 border-gray-200">
+        <div class="font-bold w-11/12 p-auto m-auto mt-6">
+            <div class="flex items-center gap-4">
+            <img class="w-10 h-10 rounded-full" src="../assets/logo.svg" alt="">
+                <div class="font-bold text-gray-700">
+                    <div>Jese Leos</div>
+                    <div class="text-sm text-gray-500 ">Joined in August 2014</div>
+                </div>
+            </div>
+        </div>
+        <ul class="space-y-2 font-medium mt-20">
+            <hr>
+            <li>
+            <a @click="toggleSidebar" href="#" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
+                <svg class="w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+                    <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
+                    <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
+                </svg>
+                <span class="ms-3">Dashboard</span>
+            </a>
+            </li>
+            <li>
+            <a @click="toggleSidebar" href="#" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-10">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6 text-gray-500">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+                <span class="flex-1 ms-3 whitespace-nowrap">Options</span>
+            </a>
+            </li>
+            <li>
+            <a @click="toggleSidebar" href="#" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
+                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"/>
+                </svg>
+                <span class="flex-1 ms-3 whitespace-nowrap">Sign Out</span>
+            </a>
+            </li>
+        </ul>
+    </div>
+</aside>
+</template>
+
+<script>
+export default {
+  name: 'LeftMenu',
+  data() {
+    return {
+      isSidebarOpen: window.innerWidth >= 640, // Initially, set sidebar state based on screen width
+    };
+  },
+  mounted() {
+    window.addEventListener('resize', this.handleResize); // Listen for window resize events
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.handleResize); // Remove event listener on component destruction
+  },
+  methods: {
+    toggleSidebar() {
+        if(window.innerWidth <= 640)
+            this.isSidebarOpen = !this.isSidebarOpen; // Toggle sidebar visibility
+    },
+    handleResize() {
+      this.isSidebarOpen = window.innerWidth >= 640; // Update sidebar state on window resize
+    },
+  },
+}
+</script>
+
--- nekizelektriko/src/views/MaybeView.vue
+++ nekizelektriko/src/views/MaybeView.vue
@@ -1,5 +1,16 @@
 <template>
-    <div>
-        why am i here
-    </div>
+  <div id="app" class="bg-green-100 h-screen w-screen">
+    <left-menu></left-menu>
+  </div>
 </template>
+
+<script>
+import LeftMenu from '../components/Menu.vue';
+
+export default {
+  name: 'App',
+  components: {
+    LeftMenu
+  }
+}
+</script>
