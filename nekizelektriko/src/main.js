debb421e53787fe2863a7b921bc1602bfbc2ac32
Ambrož Perovšek <64433302+PotatoHunter12@users.noreply.github.com>
2024-04-20 14:28:42 +0200

    added auth0 (not working)

--- nekizelektriko/src/main.js
+++ nekizelektriko/src/main.js
@@ -1,6 +1,22 @@
 import './assets/main.css'
+import { createAuth0 } from '@auth0/auth0-vue';
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+
+app.use(
+    createAuth0({
+      domain: "dev-hx4w4tj6qft5zmw0.us.auth0.com",
+      clientId: "e0kKBpxv2ssZ2JWq6LNxmp48ZL5RiqJe",
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    })
+  );
+app.use(router)
+app.config.compilerOptions.whitespace = "preserve"
+app.mount('#app')
